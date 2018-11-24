3746de576ec4bf57d8285ee76f7b23722209bae2
robbin <gooodev402@gmail.com>
2018-11-25 01:52:43 +0900

    add router and sample pages

---
+++ src/components/Menu.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink, withRouter } from "react-router-dom";
+
+const Menu = () => {
+  const activeStyle = { color: "green", fontSize: "2rem" };
+
+  return (
+    <div>
+      <ul>
+        {/* 1. link Tag */}
+        {/* <li><Link to="/">홈</Link></li>
+                <li><Link to="/about">소개</Link></li>
+                <li><Link to="/about/react">React 소개</Link></li> */}
+        {/* 2. NavLink */}
+        <li>
+          <NavLink exact to="/" activeStyle={activeStyle}>
+            홈
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/about" activeStyle={activeStyle}>
+            소개
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default withRouter(Menu);
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const uiType = ["button", "slider", "carousel", "loader"];
+const items = [
+  {
+    id: 0,
+    type: "slider",
+    inspiredBy: "10x10",
+    desc: "설명"
+  }
+];
+
+const Home = ()=>{
+    return(
+        <div>
+            <h2>홈</h2>
+        </div>
+    )
+}
+
+export default Home;
